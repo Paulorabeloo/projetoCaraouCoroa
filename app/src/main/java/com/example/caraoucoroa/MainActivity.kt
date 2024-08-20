e4fb5696117142a698f36a20048436dad50a7e85
Paulo Eduardo Rabelo De Miranda <0040482321037@americana.fatec.br>
2024-08-20 08:36:37 -0300

    CaraouCoroa

---
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.caraoucoroa
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var BotaoJogar: ImageButton
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    BotaoJogar = findViewById(R.id.botaoJogar)
+
+    BotaoJogar.setOnClickListener{
+
+        val intent = Intent(applicationContext, telaResultado::class.java)
+
+        val numero = Random.nextInt(2)
+
+        intent.putExtra("numero",numero)
+
+        startActivity(intent)
+
+    }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/caraoucoroa/telaResultado.kt
@@ -0,0 +1,52 @@
+package com.example.caraoucoroa
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class telaResultado : AppCompatActivity() {
+
+    lateinit var imagemResultado: ImageView
+    lateinit var botaoVoltar: ImageButton
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_resultado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imagemResultado = findViewById(R.id.imagemResultado)
+        botaoVoltar = findViewById(R.id.botaoVoltar)
+
+        val dados: Bundle? = intent.extras
+
+        //variavel Numero está armazenando 0 ou 1
+        val numero = dados?.getInt("numero")
+
+        if(numero == 0){
+
+           imagemResultado.setImageResource(R.drawable.moeda_cara)
+
+
+        } else {
+
+            imagemResultado.setImageResource(R.drawable.moeda_coroa)
+        }
+
+        botaoVoltar.setOnClickListener {
+            finish()
+        }
+
+
+    }
+}
